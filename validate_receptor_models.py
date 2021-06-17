Validate dimeric receptor variance, including corrected variance expression

validate_receptor_models.py
@@ -10,14 +10,63 @@ from KPR1 import model as KPR1_model
 from pysb_methods import dose_response, response_curve
 
 
+def T(params, doses):
+    K1 = params.kd1.value / params.ka1.value
+    K2 = params.kd2.value / params.ka2.value
+    K4 = params.kd4.value / params.ka4.value
+    Delta = params.R1_0.value - params.R2_0.value
+    Rt = params.R1_0.value + params.R2_0.value
+    B = 1 + K4 * (doses + K2) * (doses + K1) / (Rt * doses * K1)
+    Teq = (Rt / 2) * (B - np.sqrt(np.square(B) - 1 + (Delta / Rt)**2))
+    return Teq
+
+
+def B1(params, doses):
+    K1 = params.kd1.value / params.ka1.value
+    K2 = params.kd2.value / params.ka2.value
+    K4 = params.kd4.value / params.ka4.value
+    Delta = params.R1_0.value - params.R2_0.value
+    Rt = params.R1_0.value + params.R2_0.value
+    IFN = doses
+    t1 = -(IFN + K1) * (IFN + K2) * K4 + IFN * K1 * Delta
+    t2 = (IFN + K1) * (IFN + K2) * K4 * ((IFN + K1) * (IFN + K2) * K4 + 2 * IFN * K1 * Rt) + np.square(IFN) * K1**2 * Delta**2
+    numerator = t1 + np.sqrt(t2)
+    denominator = 2 * K1 * (IFN + K1)
+    return numerator / denominator
+
+
+def B2(params, doses):
+    K1 = params.kd1.value / params.ka1.value
+    K2 = params.kd2.value / params.ka2.value
+    K4 = params.kd4.value / params.ka4.value
+    Delta = params.R1_0.value - params.R2_0.value
+    Rt = params.R1_0.value + params.R2_0.value
+    IFN = doses
+    t1 = -(IFN + K1) * (IFN + K2) * K4 - IFN * K1 * Delta
+    t2 = (IFN + K1) * (IFN + K2) * K4 * ((IFN + K1) * (IFN + K2) * K4 + 2 * IFN * K1 * Rt) + np.square(IFN) * K1**2 * Delta**2
+    numerator = t1 + np.sqrt(t2)
+    denominator = 2 * K1 * (IFN + K2)
+    return numerator / denominator
+
+
+def dimeric_var_theory(params, doses):
+    Rt = params.R1_0.value + params.R2_0.value
+    t = T(params, doses)
+    b1 = B1(params, doses)
+    b2 = B2(params, doses)
+    term1 = Rt - 2. * t - b1 - b2
+    return 1. / (4. / (term1) + 1./t)
+
+
 if __name__ == '__main__':
-    model_type = 'KPR1'
-    plot_dr = True
+    model_type = 'dimeric'
+    plot_dr = False
     plot_rc = False
-    crange = np.logspace(0, 4, 15) * 1E-12*1E-5*6.022E23
+    plot_sigma = True
+    crange = np.logspace(-2, 4, 15) * 1E-12*1E-5*6.022E23
     koffrange = 1 / np.arange(3, 20, 2)
-    t_end = 40
-    num_traj = 50
+    t_end = 100
+    num_traj = 500
 
     # --------------------------------------------------------------------------
     if model_type == 'allosteric':
@@ -52,9 +101,30 @@ if __name__ == '__main__':
         std_traj = np.std(y['Cn'], axis=0)
 
         fig, ax = plt.subplots()
-        plt.plot(crange, mean_traj, 'k--')
+        plt.plot(crange, mean_traj, 'k--', label='simulation')
         ax.fill_between(crange, mean_traj + std_traj, mean_traj - std_traj, 'k', alpha=0.1)
+        plt.plot(crange, T(dimeric_model.parameters, crange), 'r--', label='theory')
         plt.xscale('log')
+        plt.xlabel('Ligand #')
+        plt.ylabel('T')
+        plt.legend()
+        plt.show()
+
+    if plot_sigma:
+        assert model_type == 'dimeric'
+        y = dose_response(model, crange, 'L_0', t_end, num_traj)
+        mean_traj = np.mean(y['Cn'], axis=0)
+        std_traj = np.std(y['Cn'], axis=0)
+        theory_var = dimeric_var_theory(dimeric_model.parameters, crange)
+
+        fig, ax = plt.subplots()
+        plt.plot(crange, std_traj, 'k--', label='simulation')
+        plt.plot(crange, np.sqrt(mean_traj), 'r--', label='low-concentration theory')
+        plt.plot(crange, np.sqrt(theory_var), 'b--', label='corrected theory')
+        plt.xscale('log')
+        plt.xlabel('Ligand #')
+        plt.ylabel('Standard Deviation')
+        plt.legend()
         plt.show()
 
     if plot_rc: